9b719bf1782d5b48f88d202dadb3535c960e0923
Ayush Singh <thakurayush645@gmail.com>
2025-11-23 00:02:24 +0530

    update

--- server/index.ts
+++ server/index.ts
@@ -17,7 +17,8 @@ const allowedOrigins = isSeparatedMode
   ? [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://vyora.club"
+      "https://vyora.club",
+      "https://api.vyora.club"
     ]
   : undefined;
 
